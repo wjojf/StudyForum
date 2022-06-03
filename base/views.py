d4d926c09278f847a7267b1f3ffa9e5acb004bc5
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2022-06-04 02:04:51 +0300

    Installed theme

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 import re
+from unicodedata import name
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q 
@@ -126,17 +127,23 @@ def userProfile(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all() 
     
     if request.method == 'POST':
-        form = RoomForm(request.POST)
         
-        if form.is_valid():
-            room = form.save(commit=False)   
-            room.host = request.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host=request.user,
+            topic = topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+       
+        return redirect('home')
     
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -144,18 +151,22 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
     
     if request.user != room.host:
         return HttpResponse('You are not allowed here!')
     
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-            
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
     
-    context = {'form': form}
+    context = {'form': form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
 
