6f065c0fac6bbbf80ec09c500321d4f436304961
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2022-06-06 18:29:51 +0300

    project finished!

--- base/models.py
+++ base/models.py
@@ -1,6 +1,18 @@
+from ast import Num
+from email.policy import default
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(unique=True, max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+    
+    avatar = models.ImageField(null=True, default="avatar.svg") 
+
+    USERNAME_FIELD: 'email'
+    REQUIRED_FIELDS: []
 
 
 class Topic(models.Model):
@@ -41,4 +53,4 @@ class Message(models.Model):
     def __str__(self):
         return self.body[:50]
     
-    
+
--- base/views.py
+++ base/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
 
@@ -42,10 +40,10 @@ def loginPage(request):
 
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
     
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -202,6 +200,7 @@ def updateUser(request):
     if request.method == 'POST':
         form = UserForm(
             request.POST,
+            request.FILES,
             instance=user
         )
         
